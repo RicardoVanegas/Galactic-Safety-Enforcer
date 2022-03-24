e97771a76a28c038d6a9c4ad47136307884f95ff
RicardoVanegas <a01634956@tec.mx>
2022-03-23 20:55:43 -0600

    se añadio la funcion de disparar con 'j' pero aun falta configurar unas cosas

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -12,27 +12,47 @@ public class PlayerMovement : MonoBehaviour
     public int player_charger_size = 10;
     public float player_reload_time = 1.0f;
     private Transform player_transform;
-
+    public GameObject laser;
+    public Transform firePoint;
+    private float firePointX;
+    private float firePointY;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        player_transform = GetComponent < Transform >();
+        player_transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         movement();
+        attack();
+        firePointPosition();
     }
 
-    public void movement (){
+    public void movement()
+    {
 
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        player_transform.Translate(h*player_speed*Time.deltaTime, 0, 0);
+        player_transform.Translate(h * player_speed * Time.deltaTime, 0, 0);
         player_transform.Translate(0, v * player_speed * Time.deltaTime, 0);
-        
+
+    }
+
+    public void attack()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            GameObject projectile = Instantiate(laser) as GameObject;
+            projectile.transform.position = new Vector2(firePointX, firePointY);
+        }
+    }
+    public void firePointPosition()
+    {
+        firePointX = firePoint.transform.position.x;
+        firePointY = firePoint.transform.position.y + .7f;
     }
 }
